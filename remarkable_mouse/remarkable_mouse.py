4d46e0625aed646cc7cea0289d1d8eb8c3b2822c
SBAPKat <50132527+SBAPKat@users.noreply.github.com>
2022-01-21 16:02:44 +0100

    Update remarkable_mouse.py to add sensitivity para

--- remarkable_mouse/remarkable_mouse.py
+++ remarkable_mouse/remarkable_mouse.py
@@ -122,6 +122,7 @@ def main():
         parser.add_argument('--region', action='store_true', default=False, help="Use a GUI to position the output area. Overrides --monitor")
         parser.add_argument('--threshold', metavar='THRESH', default=600, type=int, help="stylus pressure threshold (default 600)")
         parser.add_argument('--evdev', action='store_true', default=False, help="use evdev to support pen pressure (requires root, Linux only)")
+        parser.add_argument('--sensitivity', default=1, type=float, metavar='SENS', help="sensitivity multiplier, to make area 2 times smaller, use 2, etc. (default 1)")
 
         args = parser.parse_args()
 
@@ -155,6 +156,7 @@ def main():
             region=args.region,
             threshold=args.threshold,
             mode=args.mode,
+            sensitivity=args.sensitivity
         )
 
     except PermissionError:
